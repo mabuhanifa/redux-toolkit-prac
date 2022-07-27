4928332eddc7bb2d3face4d42deada91b08d7a26
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-27 23:24:09 +0600

    postSlice done

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,19 +1,33 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const fetchPosts = createAsyncThunk('post/fetchPosts', async()=>{
-const 
-})
+export const fetchPosts = createAsyncThunk("post/fetchPosts", async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return res.data;
+});
+
 const todoSlice = createSlice({
-    name : 'todo',
-    initialState : {
-        isLoading : false,
-        todos : [],
-        error : null,
-},
-extraReducer : (builder) =>{
-    builder.addCase(fetchPosts.pending,(state)=>{
-        
-    })
+  name: "todo",
+  initialState: {
+    isLoading: false,
+    todos: [],
+    error: null,
+  },
+  extraReducer: (builder) => {
+    builder.addCase(fetchPosts.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchPosts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.todos = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchPosts.rejected, (state, action) => {
+      state.isLoading = false;
+      state.todos = [];
+      state.error = action.error.message;
+    });
+  },
+});
 
-}
-})
+export default todoSlice.reducer;
