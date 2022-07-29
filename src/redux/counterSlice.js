b753c9d91777bc02b41fc88d8f12b923e9527004
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-29 17:01:09 +0600

    counterSlice added

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    counter: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.counter += 1;
+    },
+    decrement: (state) => {
+      state.counter += 1;
+    },
+    incrementByValue: (state, action) => {
+      state.counter += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByValue } = counterSlice.actions;
+export default counterSlice.reducer;
